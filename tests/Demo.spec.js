151d22abb56713a44e46fb017e95cf02a857fa71
Bakii-QA <kornrawee5678@gmail.com>
2026-06-13 21:07:31 +0700

    redo

--- tests/Demo.spec.js
+++ tests/Demo.spec.js
@@ -1,21 +1,21 @@
-const {test, beforeEach} = require('@playwright/test')
+// const {test, beforeEach} = require('@playwright/test')
 
-test.describe('test function login',async()=>{
-    beforeEach(async({page})=>{
-            await page.goto('assadadsasd');
-    });
+// test.describe('test function login',async()=>{
+//     beforeEach(async({page})=>{
+//             await page.goto('assadadsasd');
+//     });
 
-    test('กรอก username/pass',async({page})=>{
-        await page.locator("#sdaad").fill("username");
-        await page.locator("#pass").fill("password");
-        await page.locator("#signin").click();
+//     test('กรอก username/pass',async({page})=>{
+//         await page.locator("#sdaad").fill("username");
+//         await page.locator("#pass").fill("password");
+//         await page.locator("#signin").click();
 
-        const aa = page.locator("#danger")
-        await expect(aa).toBeVisible();
-        await expect(aa).toContext("asdda");
+//         const aa = page.locator("#danger")
+//         await expect(aa).toBeVisible();
+//         await expect(aa).toContext("asdda");
 
 
-    });
+//     });
 
 
 

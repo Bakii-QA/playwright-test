f37a2635dc370466bfe24cd2b08a280e99fef224
Bakii-QA <kornrawee5678@gmail.com>
2026-06-29 15:35:25 +0700

    redo

---
+++ tests/Mobile888.spec.js
@@ -0,0 +1,26 @@
+const {test , expect} = required('@playwright/test');
+
+test.describe("เทสการเปิดเบอร์ 888",async()=>{
+
+
+    test("เปิด E2E",async({page})=>{
+        await page.goto("https://sit-mychannel.cdc.ais.th/ais-fibre/login/callback-signin");
+        await page.locator("select.form-control").selectOption("Temporary login");
+        await page.locator(".button-base").click();
+
+
+
+
+
+
+
+
+    });
+
+
+
+
+
+
+
+});

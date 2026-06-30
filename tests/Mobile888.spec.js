fe846fdda7a327a85b667e308ff29ead9dc6ca03
Bakii-QA <kornrawee5678@gmail.com>
2026-06-30 10:10:15 +0700

    redo

--- tests/Mobile888.spec.js
+++ tests/Mobile888.spec.js
@@ -17,7 +17,7 @@ test.describe("เทสการเปิดเบอร์ 888",async()=>{
         await page.getByTestId('login-page-continue-login-button').click();
         await page.waitForLoadState('networkidle');
         await page.goto("https://sit-mychannel.cdc.ais.th/ais-fibre/?type=logged-in");
-        await page.getByText(" ปิด ").click();    
+        await page.getByText(/ปิด/i).click();    
         const optionLocator = page.locator('.option.right-block');
         await optionLocator.waitFor({ state: 'visible', timeout: 15000 }); // รอสูงสุด 15 วินาที
         await optionLocator.click();        await page.getByText(/คอนโด/i).waitFor({ state: 'visible' });

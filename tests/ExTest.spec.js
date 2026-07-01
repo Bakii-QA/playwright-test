217f0e53680b863fc60cd0b2c918a99180c336b3
Bakii-QA <kornrawee5678@gmail.com>
2026-07-01 13:02:24 +0700

    redo

--- tests/ExTest.spec.js
+++ tests/ExTest.spec.js
@@ -16,7 +16,7 @@ test.describe('ทดสอบการไหล Flow',()=>{
     test('TC02-Change Language',async ({page})=>{
         await page.locator('#language').click();
         await page.locator('[role="menuitem"]',{hasText:'DE'}).click();
-        await page.locator('[role="menuitem"]',{hasText:'EN'}).click();
+        await page.locator('.dropdown-item [role="menuitem"]',{hasText:'EN'}).click();
     })
     test('TC03-SignIn Account',async({page})=>{
         await page.locator('[data-test="nav-sign-in"]').click();

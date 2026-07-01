39d071b5db149aa48803fcc39b95f4899f1ad85d
Bakii-QA <kornrawee5678@gmail.com>
2026-07-01 13:40:37 +0700

    redo

--- tests/ExTest.spec.js
+++ tests/ExTest.spec.js
@@ -17,8 +17,8 @@ test.describe('ทดสอบการไหล Flow',()=>{
         await page.locator('#language').click();
         await page.locator('[role="menuitem"]',{hasText:'DE'}).click();
 
-        await page.keyboard.press('Escape');
-        
+        await page.keyboard.press('Escape'); // บังคับให้หน้าเว็บ Reset สถานะเมนู
+
         await page.locator('#language').click();
         //await page.locator('.dropdown-item [role="menuitem"]',{hasText:'EN'}).click();
         await page.getByRole('menuitem',{name: 'EN' }).click();
@@ -27,7 +27,16 @@ test.describe('ทดสอบการไหล Flow',()=>{
     test('TC03-SignIn Account',async({page})=>{
         await page.locator('[data-test="nav-sign-in"]').click();
         await page.locator('input[type="email"]').fill('banktest@gmail.com');
+        const passwordField = page.locator('input[type="password"]')
+        await passwordField.fill('123');
+        await page.locator('input[type="button"]').click();
 
+        const currentValue = await passwordField.inputValue();
+        await page.locator('input[type="password"]').fill(currentValue+'456');
+        //page.keyboard.type('123'); เป็นคำสั่งพิมต่อจากข้อความเดิมเช่น
+        // await page.keyboard.type('123'); // พิมพ์ 123
+        // await page.locator('input[type="button"]').click(); // กดปุ่ม
+        // await page.keyboard.type('456'); // พิมพ์ 456 ต่อเข้าไป (ผลลัพธ์จะเป็น 123456)
 
 
     })

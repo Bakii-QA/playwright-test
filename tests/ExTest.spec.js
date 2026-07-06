224b5d9d49655724de2f9c8bb8a96e1c9c19c552
Bakii-QA <kornrawee5678@gmail.com>
2026-07-06 14:11:42 +0700

    redo

--- tests/ExTest.spec.js
+++ tests/ExTest.spec.js
@@ -136,7 +136,7 @@ test.describe('ทดสอบการไหล Flow',()=>{
             await page.screenshot({ path: 'failure-state.png' });
             throw new Error("ระบบไม่ยอมเปลี่ยนหน้าหลังจากกด Register (อาจเกิดจาก API Error หรือหน้าเว็บค้าง)");
         }
-
+        console.log("URL ปัจจุบันคือ:", currentUrl);
     //     await page.locator('input[data-test="email"]').fill(email);
     //    await page.locator('input[data-test="password"]').fill(password);
     //    await page.locator('button[data-test="login-submit"]').click();
